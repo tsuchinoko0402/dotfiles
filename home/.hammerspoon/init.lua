638c95c28412387b75eb647cb7c8a3fbaa5c4697
tsuchinoko0402 <okazaki@zakioka.net>
2022-12-25 19:35:43 +0900

    alacritty を ctrl 2回押しで表示/非表示を切り替える設定を追加

---
+++ home/.hammerspoon/init.lua
@@ -0,0 +1,24 @@
+local application = require("hs.application")
+local spaces = require("hs.spaces")
+
+local double_press = require("ctrlDoublePress")
+
+-- double tap で toggle で Aracritty を表示/非表示する
+local open_alacritty = function()
+  local appName = "Alacritty"
+  local app = application.get(appName)
+  
+  if app == nil then
+    application.launchOrFocus(appName)
+  elseif app:isFrontmost() then
+    app:hide()
+  else
+    local active_space = spaces.focusedSpace()
+    local alacritty_win = app:focusedWindow()
+    spaces.moveWindowToSpace(alacritty_win, active_space)
+    app:setFrontmost()
+  end
+end
+
+double_press.timeFrame = 0.5
+double_press.action = open_alacritty
