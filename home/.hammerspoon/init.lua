0b099266fcf02e048af595a3f34183ba58077fe7
OKAZAKI Shogo <okazaki@zakioka.net>
2024-06-18 22:14:44 +0900

    hammerspoon の設定を変更

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -3,9 +3,9 @@ local spaces = require("hs.spaces")
 
 local double_press = require("ctrlDoublePress")
 
--- double tap で toggle で Aracritty を表示/非表示する
-local open_alacritty = function()
-  local appName = "Warp"
+-- double tap で toggle で WezTerm を表示/非表示する
+local open_wezterm = function()
+  local appName = "WezTerm"
   local app = application.get(appName)
   
   if app == nil then
@@ -14,11 +14,11 @@ local open_alacritty = function()
     app:hide()
   else
     local active_space = spaces.focusedSpace()
-    local alacritty_win = app:focusedWindow()
-    spaces.moveWindowToSpace(alacritty_win, active_space)
+    local wezterm_win = app:focusedWindow()
+    spaces.moveWindowToSpace(wezterm_win, active_space)
     app:setFrontmost()
   end
 end
 
 double_press.timeFrame = 0.5
-double_press.action = open_alacritty
+double_press.action = open_wezterm
