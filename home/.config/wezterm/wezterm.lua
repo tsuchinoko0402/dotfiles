d2099213b106db3af0b706e2dff01c2ff47e9f23
OKAZAKI Shogo <okazaki@zakioka.net>
2024-11-22 01:12:15 +0900

    wezterm 設定ファイルを更新

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -59,52 +59,147 @@ end)
 
 -- ショートカットキー設定
 local act = wezterm.action
+
+-- キーバインド
 config.keys = {
-  -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
-  {
-    key = 'f',
-    mods = 'SHIFT|META',
-    action = wezterm.action.ToggleFullScreen,
-  },
-  -- Ctrl+Shift+tで新しいタブを作成
-  {
-    key = 't',
-    mods = 'SHIFT|CTRL',
-    action = act.SpawnTab 'CurrentPaneDomain',
-  },
-  -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
-  {
-    key = 'd',
-    mods = 'SHIFT|CTRL',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  -- Ctrl+左矢印でカーソルを前の単語に移動
-  {
-    key = "LeftArrow",
-    mods = "CTRL",
-    action = act.SendKey {
-      key = "b",
-      mods = "META",
+    -- ¥ではなく、バックスラッシュを入力する。おそらくMac固有
+    {
+        key = "¥",
+        action = wezterm.action.SendKey { key = '\\' }
     },
-  },
-  -- Ctrl+右矢印でカーソルを次の単語に移動s
-  {
-    key = "RightArrow",
-    mods = "CTRL",
-    action = act.SendKey {
-      key = "f",
-      mods = "META",
+    -- Altを押した場合はバックスラッシュではなく¥を入力する。おそらくMac固有
+    {
+        key = "¥",
+        mods = "ALT",
+        action = wezterm.action.SendKey { key = '¥' }
     },
-  },
-  -- Ctrl+Backspaceで前の単語を削除
-  {
-    key = "Backspace",
-    mods = "CTRL",
-    action = act.SendKey {
-      key = "w",
+    -- ⌘ + でフォントサイズを大きくする
+    {
+        key = "+",
+        mods = "CMD|SHIFT",
+        action = wezterm.action.IncreaseFontSize,
+    },
+    -- ⌘ w でペインを閉じる（デフォルトではタブが閉じる）
+    {
+        key = "w",
+        mods = "CMD",
+        action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    -- ⌘ Ctrl ,で下方向にペイン分割
+    {
+        key = ",",
+        mods = "CMD|CTRL",
+        action = wezterm.action { SplitVertical = { domain = "CurrentPaneDomain" } },
+    },
+    -- ⌘ Ctrl .で右方向にペイン分割
+    {
+        key = ".",
+        mods = "CMD|CTRL",
+        action = wezterm.action { SplitHorizontal = { domain = "CurrentPaneDomain" } },
+    },
+    -- ⌘ Ctrl oでペインの中身を入れ替える
+    {
+        key = "o",
+        mods = "CMD|CTRL",
+        action = wezterm.action.RotatePanes 'Clockwise'
+    },
+    -- ⌘ Ctrl hjklでペインの移動
+    {
+        key = 'h',
+        mods = 'CMD|CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'j',
+        mods = 'CMD|CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Down',
+    },
+    {
+        key = 'k',
+        mods = 'CMD|CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'l',
+        mods = 'CMD|CTRL',
+        action = wezterm.action.ActivatePaneDirection 'Right',
+    },
+    -- ⌘ Ctrl Shift hjklでペイン境界の調整
+    {
+        key = 'h',
+        mods = 'CMD|CTRL|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Left', 2 },
+    },
+    {
+        key = 'j',
+        mods = 'CMD|CTRL|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Down', 2 },
+    },
+    {
+        key = 'k',
+        mods = 'CMD|CTRL|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Up', 2 },
+    },
+    {
+        key = 'l',
+        mods = 'CMD|CTRL|SHIFT',
+        action = wezterm.action.AdjustPaneSize { 'Right', 2 },
+    },
+    -- Alt(Opt)+Shift+Fでフルスクリーン切り替え
+    {
+      key = 'f',
+      mods = 'SHIFT|META',
+      action = wezterm.action.ToggleFullScreen,
+    },
+    -- Ctrl+Shift+tで新しいタブを作成
+    {
+      key = 't',
+      mods = 'SHIFT|CTRL',
+      action = act.SpawnTab 'CurrentPaneDomain',
+    },
+    -- Ctrl+Shift+dで新しいペインを作成(画面を分割)
+    {
+      key = 'd',
+      mods = 'SHIFT|CTRL',
+      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    -- Ctrl+左矢印でカーソルを前の単語に移動
+    {
+      key = "LeftArrow",
       mods = "CTRL",
+      action = act.SendKey {
+        key = "b",
+        mods = "META",
+      },
     },
-  },
+    -- Ctrl+右矢印でカーソルを次の単語に移動s
+    {
+      key = "RightArrow",
+      mods = "CTRL",
+      action = act.SendKey {
+        key = "f",
+        mods = "META",
+      },
+    },
+    -- Ctrl+Backspaceで前の単語を削除
+    {
+      key = "Backspace",
+      mods = "CTRL",
+      action = act.SendKey {
+        key = "w",
+        mods = "CTRL",
+      },
+    },
+ }
+
+-- マウス操作の挙動設定
+config.mouse_bindings = {
+    -- 右クリックでクリップボードから貼り付け
+    {
+        event = { Down = { streak = 1, button = 'Right' } },
+        mods = 'NONE',
+        action = wezterm.action.PasteFrom 'Clipboard',
+    },
 }
 
 -- tab bar
